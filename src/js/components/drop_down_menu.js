d849c0eba24740b04454bfbf825c432754d177bd
whatisaProCoder <debnathpritam2005@gmail.com>
2025-08-17 00:06:53 +0530

    mvp

---
+++ src/js/components/drop_down_menu.js
@@ -0,0 +1,147 @@
+import Scrollbar from "smooth-scrollbar";
+
+export class CustomDropDownMenu {
+  constructor({
+    menuTitle,
+    triggerElementID,
+    actionItemArray,
+    height,
+    width,
+    logEvent,
+  }) {
+    this.menuTitle = menuTitle;
+    this.triggerElementID = triggerElementID;
+    this.actionItemArray = actionItemArray;
+    this.height = parseInt(height.slice(0, -2));
+    this.width = parseInt(width.slice(0, -2));
+    this.isOpen = false;
+    this.logEvent = logEvent;
+  }
+  #calculatePosition() {
+    const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth;
+
+    this.triggerElementRect = this.triggerElement.getBoundingClientRect();
+    console.log(this.triggerElementRect);
+
+    const triggerPositionX =
+      this.triggerElementRect.left + this.triggerElementRect.width / 2;
+    const triggerPositionY =
+      this.triggerElementRect.top + this.triggerElementRect.height / 2;
+
+    let menuPositionX = null;
+    let menuPositionY = null;
+
+    if (windowWidth - triggerPositionX > this.width + 32) {
+      menuPositionX = triggerPositionX + 32;
+    } else {
+      menuPositionX = windowWidth - this.width - 32;
+    }
+
+    if (windowHeight - triggerPositionY > this.height + 32) {
+      menuPositionY = triggerPositionY + 32;
+    } else {
+      menuPositionY = windowHeight - this.height - 32;
+    }
+
+    console.log(menuPositionX, menuPositionY);
+
+    menuPositionX = Math.ceil(menuPositionX);
+    menuPositionY = Math.ceil(menuPositionY);
+
+    return { menuPositionX, menuPositionY };
+  }
+  setEventListeners() {
+    this.triggerElement = document.querySelector(`#${this.triggerElementID}`);
+    this.triggerElement.addEventListener("click", (event) => {
+      if (this.logEvent == true) console.log(event.target);
+      if (this.isOpen == false) {
+        this.isOpen = true;
+        this.render();
+      }
+    });
+  }
+  render() {
+    if (this.logEvent == true)
+      console.log("Opening Menu of Title: ", this.menuTitle);
+
+    const { menuPositionX, menuPositionY } = this.#calculatePosition();
+    this.menuElement = document.createElement("div");
+
+    this.menuElement.style.position = "fixed";
+    this.menuElement.style.height = `${this.height}px`;
+    this.menuElement.style.width = `${this.width}px`;
+    this.menuElement.style.left = `${menuPositionX}px`;
+    this.menuElement.style.top = `${menuPositionY}px`;
+    this.menuElement.style.overflow = "hidden";
+
+    this.menuElement.classList.add("border-1", "border-[#D7D7D7]");
+    this.menuElement.classList.add("bg-[#F5F5F5]", "rounded-md");
+    this.menuElement.classList.add("shadow-[0px_4px_16px_rgba(0,0,0,15%)]");
+    this.menuElement.classList.add("flex", "flex-col");
+
+    this.menuElement.innerHTML = /* html */ `
+            <div class="font-semibold text-xl text-[#3C3C3C] px-4 py-2.5 border-b-1 border-[#D7D7D7]">${this.menuTitle}</div>
+            <div id="action-group">
+            </div>
+        `;
+
+    const actionGroup = this.menuElement.querySelector("#action-group");
+    actionGroup.style.overflow = "auto";
+    this.actionItemArray.forEach((actionItem) => {
+      const actionItemElement = document.createElement("div");
+      actionItemElement.classList.add("p-4", "py-2.5", "mt-2");
+      actionItemElement.style.userSelect = "none";
+      actionItemElement.innerHTML = /* html */ `
+                ${actionItem.actionName}
+            `;
+      actionItemElement.addEventListener("click", () => {
+        actionItem.actionFunction();
+        this.close();
+      });
+      actionGroup.appendChild(actionItemElement);
+    });
+
+    document.body.appendChild(this.menuElement);
+
+    document.addEventListener("click", (event) => {
+      if (
+        !this.menuElement.contains(event.target) &&
+        this.isOpen == true &&
+        !this.triggerElement.contains(event.target)
+      ) {
+        this.close();
+      }
+    });
+
+    Scrollbar.init(this.menuElement.querySelector("#action-group"), {
+      alwaysShowTracks: true,
+    });
+
+    this.menuElement.classList.remove("fade-out");
+    this.menuElement.classList.add("fade-in");
+  }
+  close() {
+    if (this.logEvent == true) {
+      console.log("Closing Menu of Title: ", this.menuTitle);
+    }
+    this.menuElement.classList.remove("fade-in");
+    this.menuElement.classList.add("fade-out");
+    setTimeout(() => {
+      this.menuElement.remove();
+      this.isOpen = false;
+    }, 500);
+  }
+}
+
+export class ActionItem {
+  constructor(actionName, actionFunction) {
+    this.actionName = actionName;
+    this.actionFunction = actionFunction;
+  }
+  addIcon(iconSrc, iconScale) {
+    this.iconSrc = iconSrc;
+    this.iconScale = iconScale;
+    return this;
+  }
+}
--- src/js/demo/page.js
+++ src/js/demo/page.js
@@ -1,4 +1,5 @@
 import threeDotIcon from "../../icons/three-dot-icon.svg";
+import { ActionItem, CustomDropDownMenu } from "../components/drop_down_menu";
 
 export default function createDemoPage() {
   const content = document.querySelector(".content");
@@ -6,17 +7,40 @@ export default function createDemoPage() {
   console.log("Creating demo page...");
   const demoPage = document.createElement("div");
   demoPage.classList.add("demo-page");
+  demoPage.classList.add(
+    "flex",
+    "items-center",
+    "justify-center",
+    "h-screen",
+    "w-screen",
+    "bg-[#E1E1E1]",
+  );
 
   demoPage.innerHTML = /* html */ `
-        <div class="demo-page ">
-            <div class="demo-card">
-                <div class="heading">Custom Dropdown Menu</div>
-                <div class="chip">Click on the Three-Dot-Menu Icon</div>
-                <img class="dropdown-menu-button" src="${threeDotIcon}"> 
-                <div class="credits">Made with <span>VanillaJS, by whatisaProCoder</div>
-            </div>
+        <div class="demo-card flex flex-col items-center gap-1 border-1 border-[#d7d7d7] bg-[#f5f5f5] rounded-md shadow-[0px_46px_130px_rgba(0,0,0,15%)]">
+            <div class="border-b-1 border-b-[#D7D7D7] px-10 py-4 text-3xl text-center font-semibold w-max">Custom Dropdown Menu</div>
+            <div class="mt-6 bg-[#AC1CFF] p-1 px-2 rounded-md text-[#F5E4F5] font-semibold text-s">Click on the Three-Dot-Menu Icon</div>
+            <img id="three-dot-menu-button" class="h-12 w-12 bg-[#E1E1E1] mt-12 p-2 rounded-full hover:bg-[#D7D7D7] active:bg-[#CDCDCD] transition" src="${threeDotIcon}"> 
+            <div class="credits mt-12 mb-4">Made with <span class="text-[#FF9500] font-medium">VanillaJS</span>, by <a href="https://github.com/whatisaProCoder" target="_blank" class="text-[#6E88FB] font-medium hover:underline">whatisaProCoder</a></div>
         </div>
     `;
 
   content.appendChild(demoPage);
+
+  const menu = new CustomDropDownMenu({
+    menuTitle: "Menu",
+    triggerElementID: "three-dot-menu-button",
+    actionItemArray: [
+      new ActionItem("Action 1", () => console.log("Action 1 pressed")),
+      new ActionItem("Action 2", () => console.log("Action 2 pressed")),
+      new ActionItem("Action 3", () => console.log("Action 3 pressed")),
+      new ActionItem("Action 4", () => console.log("Action 4 pressed")),
+      new ActionItem("Action 5", () => console.log("Action 5 pressed")),
+    ],
+    height: "215px",
+    width: "180px",
+    logEvent: true,
+  });
+
+  menu.setEventListeners({ logEvent: true });
 }
